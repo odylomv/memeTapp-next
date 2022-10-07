e4b39f80ae0e9fd5ecd44b8b63a5af45ddd373c7
Odysseas Lomvardeas <odylomv@gmail.com>
2022-10-07 17:34:09 +0300

    remove getAll query

--- src/server/trpc/router/meme.ts
+++ src/server/trpc/router/meme.ts
@@ -11,18 +11,13 @@ export const memeRouter = t.router({
       }),
     };
   }),
-  getAll: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.meme.findMany();
-  }),
   getPaginated: t.procedure
     .input(z.object({ limit: z.number().min(1), cursor: z.number().min(1).nullish() }))
     .query(async ({ input, ctx }) => {
       const memes = await ctx.prisma.meme.findMany({
         take: input.limit,
         cursor: input.cursor ? { id: input.cursor } : undefined,
-        orderBy: {
-          id: 'desc',
-        },
+        orderBy: { id: 'desc' },
         include: {
           author: true,
           _count: {
