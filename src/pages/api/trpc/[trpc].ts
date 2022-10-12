a4317ccaf6f7db69b1c62e4c4c4033187d00732a
Odysseas Lomvardeas <odylomv@gmail.com>
2022-10-13 02:16:41 +0300

    add trpc onError like the new versions of ct3a

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,6 @@
 // src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { env } from '../../../env/server.mjs';
 import { createContext } from '../../../server/trpc/context';
 import { appRouter } from '../../../server/trpc/router';
 
@@ -7,4 +8,10 @@ import { appRouter } from '../../../server/trpc/router';
 export default createNextApiHandler({
   router: appRouter,
   createContext,
+  onError:
+    env.NODE_ENV === 'development'
+      ? ({ path, error }) => {
+          console.error(`❌ tRPC failed on ${path}: ${error}`);
+        }
+      : undefined,
 });
